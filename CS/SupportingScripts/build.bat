c99031d0243a2202e3420f3c70f7b2856f64f93f
Tharinda Liyanage <tharindakasun@gmail.com>
2017-01-16 12:46:47 +0530

    git pull added to build bat

--- CS/SupportingScripts/build.bat
+++ CS/SupportingScripts/build.bat
@@ -1,12 +1,14 @@
 set location="C:\GIT\Telerik"
 
 @echo off
+
 set hh=%time:~0,2%
 if "%time:~0,1%"==" " set hh=0%hh:~1,1%   
 set dt=%date:~4,2%-%date:~7,2%-%date:~10,4%_%hh:~0,2%_%time:~3,2%_%time:~6,2%
 
 taskkill /F /IM vstest.discoveryengine.x86.exe /FI "MEMUSAGE gt 1"
 cd %location%
+call pull.bat
 "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "C:\GIT\Telerik\CS.sln" /p:configuration=debug
 
 cd "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE"
