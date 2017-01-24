7d1091db14377d133007add0fc5d01586f597e44
Tharinda Liyanage <tharindakasun@gmail.com>
2017-01-24 16:43:30 +0530

    added a git sync title to the log

--- CS/SupportingScripts/pull.bat
+++ CS/SupportingScripts/pull.bat
@@ -1,5 +1,5 @@
 @echo off
-echo %DATE% %TIME% >C:\GIT\Telerik\CS\SupportingScripts\pull.log
+echo *** Git Sync info: %DATE% %TIME% >C:\GIT\Telerik\CS\SupportingScripts\pull.log
 cd C:\GIT\Telerik
 git reset --hard HEAD >>C:\GIT\Telerik\CS\SupportingScripts\pull.log
 git pull >>C:\GIT\Telerik\CS\SupportingScripts\pull.log
