5af59792f77b3404170ccb2cc830cf7178031ccd
Tharinda Liyanage <tharindakasun@gmail.com>
2017-02-10 20:25:57 +0530

    Update the logic handling the "New Item" identification

--- CS/CommonMethods/Utilities.cs
+++ CS/CommonMethods/Utilities.cs
@@ -108,15 +108,16 @@ namespace CS.CommonMethods
         {
             m.ActiveBrowser.RefreshDomTree();
             int counter0 = 0;
-            //HtmlDiv newItemIcon = m.ActiveBrowser.Find.ById<HtmlDiv>("HtmlPage_newItem");
-            TopMenu tm = new TopMenu(m);
-            while (tm.newItemIcon == null && counter0 < 10) //this will try upto 10 times before fails
+            HtmlDiv newItemIcon = m.ActiveBrowser.Find.ById<HtmlDiv>("HtmlPage_newItem");
+            //TopMenu tm = new TopMenu(m);
+            while (newItemIcon == null && counter0 < 10) //this will try upto 10 times before fails
             {
                 Thread.Sleep(config.Default.SleepingTime * 10);
-                counter0 += 1;
+               
                 m.ActiveBrowser.RefreshDomTree();
-                //newItemIcon = m.ActiveBrowser.Find.ById<HtmlDiv>("HtmlPage_newItem");
-                tm = new TopMenu(m);
+                newItemIcon = m.ActiveBrowser.Find.ById<HtmlDiv>("HtmlPage_newItem");
+                counter0 += 1;
+               // tm = new TopMenu(m);
             }
         }
 
